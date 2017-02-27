9e47b789e4629152284f598d8a28ebf5969143aa
skwalkerlee <lzy460332595@gmail.com>
2017-02-27 15:32:58 +0800

    添加config接口

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"os"
+
+	"github.com/BurntSushi/toml"
+	"github.com/astaxie/beego/logs"
+)
+
+type Config struct {
+	Etcd struct {
+		Addr []string `toml:"addr"`
+	} `toml:"etcd"`
+	Omq struct {
+		Clientport string `toml:"clientport"`
+		Innerport  string `toml:"innerport"`
+		Timeout    int    `toml:"timeout"`
+		Logdir     string `toml:"logdir"`
+	} `toml:"omq"`
+}
+
+var Conf Config
+
+func init() {
+	if _, err := toml.DecodeFile("./omq.conf", &Conf); err != nil {
+		logs.Error(err)
+		os.Exit(1)
+	}
+}
